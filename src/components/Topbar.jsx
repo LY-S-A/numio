aba67afcb112e9ea0b02f0c859a348255e7a9034
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-13 17:20:15 +0100

    Update Topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,3 +1,50 @@
+// import {
+//   FaBell,
+//   FaWallet,
+//   FaBars,
+// } from "react-icons/fa";
+
+// import { useBalance } from "../context/BalanceContext";
+
+// import "../styles/topbar.css";
+
+// const Topbar = ({ setSidebarOpen }) => {
+//   const { formattedBalance } = useBalance();
+
+//   return (
+//     <header className="topbar">
+//       <div className="topbar-left">
+//         <button
+//           className="hamburger"
+//           onClick={() => setSidebarOpen(true)}
+//         >
+//           <FaBars />
+//         </button>
+
+//         <div className="welcome-text">
+//           {/* <p>Welcome back,</p> */}
+//           <h1>Hi Olalekan 👋</h1>
+//         </div>
+//       </div>
+
+//       <div className="topbar-right">
+//         <div className="wallet-box">
+//           <div>
+//             <span>Wallet Balance</span>
+//             <h3>{formattedBalance}</h3>
+//           </div>
+
+//           <FaWallet />
+//         </div>
+
+//         <FaBell className="top-icon" />
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Topbar;
+
 import {
   FaBell,
   FaWallet,
@@ -9,7 +56,10 @@ import { useBalance } from "../context/BalanceContext";
 import "../styles/topbar.css";
 
 const Topbar = ({ setSidebarOpen }) => {
-  const { formattedBalance } = useBalance();
+  const {
+    formattedBalance,
+    loadingBalance,
+  } = useBalance();
 
   return (
     <header className="topbar">
@@ -31,7 +81,10 @@ const Topbar = ({ setSidebarOpen }) => {
         <div className="wallet-box">
           <div>
             <span>Wallet Balance</span>
-            <h3>{formattedBalance}</h3>
+
+            <h3>
+              {loadingBalance ? "Loading..." : formattedBalance}
+            </h3>
           </div>
 
           <FaWallet />
