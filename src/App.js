9eeffb22457a4052a2c731f70d00badd1b90df19
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-13 16:44:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,105 @@
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+
+// import { ThemeProvider } from "./context/ThemeContext";
+
+// import Layout from "./components/Layout";
+
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import ForgotPassword from "./pages/ForgotPassword";
+// import ResetPassword from "./pages/ResetPassword";
+
+// import Dashboard from "./pages/Dashboard";
+// import BuyNumber from "./pages/BuyNumber";
+// import Inbox from "./pages/Inbox";
+// import FundWallet from "./pages/FundWallet";
+// import Transactions from "./pages/Transactions";
+// import Support from "./pages/Support";
+// import Settings from "./pages/Settings";
+// import Appearance from "./pages/Appearance";
+
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <ThemeProvider>
+//       <BrowserRouter>
+//         <Routes>
+//           {/* Auth Pages */}
+//           <Route
+//             path="/"
+//             element={<Login />}
+//           />
+
+//           <Route
+//             path="/register"
+//             element={<Register />}
+//           />
+
+//           <Route
+//             path="/forgot-password"
+//             element={<ForgotPassword />}
+//           />
+
+//           <Route
+//             path="/reset-password/:token"
+//             element={<ResetPassword />}
+//           />
+
+//           {/* Protected Pages */}
+//           <Route element={<Layout />}>
+//             <Route
+//               path="/dashboard"
+//               element={<Dashboard />}
+//             />
+
+//             <Route
+//               path="/buy-number"
+//               element={<BuyNumber />}
+//             />
+
+//             <Route
+//               path="/inbox"
+//               element={<Inbox />}
+//             />
+
+//             <Route
+//               path="/fund-wallet"
+//               element={<FundWallet />}
+//             />
+
+//             <Route
+//               path="/transactions"
+//               element={<Transactions />}
+//             />
+
+//             <Route
+//               path="/support"
+//               element={<Support />}
+//             />
+
+//             <Route
+//               path="/settings"
+//               element={<Settings />}
+//             />
+
+//             <Route
+//               path="/appearance"
+//               element={<Appearance />}
+//             />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </ThemeProvider>
+//   );
+// }
+
+// export default App;
+
 import {
   BrowserRouter,
   Routes,
@@ -5,6 +107,7 @@ import {
 } from "react-router-dom";
 
 import { ThemeProvider } from "./context/ThemeContext";
+import { BalanceProvider } from "./context/BalanceContext";
 
 import Layout from "./components/Layout";
 
@@ -27,73 +130,75 @@ import "./App.css";
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Auth Pages */}
-          <Route
-            path="/"
-            element={<Login />}
-          />
-
-          <Route
-            path="/register"
-            element={<Register />}
-          />
-
-          <Route
-            path="/forgot-password"
-            element={<ForgotPassword />}
-          />
-
-          <Route
-            path="/reset-password/:token"
-            element={<ResetPassword />}
-          />
-
-          {/* Protected Pages */}
-          <Route element={<Layout />}>
+      <BalanceProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* Auth Pages */}
             <Route
-              path="/dashboard"
-              element={<Dashboard />}
+              path="/"
+              element={<Login />}
             />
 
             <Route
-              path="/buy-number"
-              element={<BuyNumber />}
+              path="/register"
+              element={<Register />}
             />
 
             <Route
-              path="/inbox"
-              element={<Inbox />}
+              path="/forgot-password"
+              element={<ForgotPassword />}
             />
 
             <Route
-              path="/fund-wallet"
-              element={<FundWallet />}
+              path="/reset-password/:token"
+              element={<ResetPassword />}
             />
 
-            <Route
-              path="/transactions"
-              element={<Transactions />}
-            />
+            {/* Protected Pages */}
+            <Route element={<Layout />}>
+              <Route
+                path="/dashboard"
+                element={<Dashboard />}
+              />
 
-            <Route
-              path="/support"
-              element={<Support />}
-            />
+              <Route
+                path="/buy-number"
+                element={<BuyNumber />}
+              />
 
-            <Route
-              path="/settings"
-              element={<Settings />}
-            />
+              <Route
+                path="/inbox"
+                element={<Inbox />}
+              />
 
-            <Route
-              path="/appearance"
-              element={<Appearance />}
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+              <Route
+                path="/fund-wallet"
+                element={<FundWallet />}
+              />
+
+              <Route
+                path="/transactions"
+                element={<Transactions />}
+              />
+
+              <Route
+                path="/support"
+                element={<Support />}
+              />
+
+              <Route
+                path="/settings"
+                element={<Settings />}
+              />
+
+              <Route
+                path="/appearance"
+                element={<Appearance />}
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </BalanceProvider>
     </ThemeProvider>
   );
 }
