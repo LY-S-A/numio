9d4e661741eb93c8ee89931ec04d8c23e6e00cc6
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-20 14:36:23 +0100

    Update BalanceContext.jsx

--- src/context/BalanceContext.jsx
+++ src/context/BalanceContext.jsx
@@ -1,32 +1,3 @@
-// import { createContext, useContext, useState } from "react";
-
-// const BalanceContext = createContext();
-
-// export const BalanceProvider = ({ children }) => {
-//   const [balance, setBalance] = useState(20000);
-
-//   // Format as Nigerian Naira
-//   const formattedBalance = new Intl.NumberFormat("en-NG", {
-//     style: "currency",
-//     currency: "NGN",
-//     minimumFractionDigits: 2,
-//   }).format(balance);
-
-//   return (
-//     <BalanceContext.Provider
-//       value={{
-//         balance,
-//         formattedBalance,
-//         setBalance,
-//       }}
-//     >
-//       {children}
-//     </BalanceContext.Provider>
-//   );
-// };
-
-// export const useBalance = () => useContext(BalanceContext);
-
 import {
   createContext,
   useContext,
