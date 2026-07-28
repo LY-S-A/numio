1d7175022a0641be76137baf91e36452f68fd090
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-28 09:32:14 +0100

    Update FundSuccess.jsx

--- src/pages/FundSuccess.jsx
+++ src/pages/FundSuccess.jsx
@@ -34,7 +34,7 @@ const FundSuccess = () => {
   return (
     <div className="fund-status-page">
       
-      <div className="fund-status-card success">
+      <div className="fund-status-card success-card">
 
         <div className="shape shape1"></div>
         <div className="shape shape2"></div>
