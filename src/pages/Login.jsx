98acf976d5f5743cf09b67fc3c642c17b71b8bc2
lysa.waitlist <Lysauniverse30@gmail.com>
2026-08-12 18:38:59 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -77,37 +77,6 @@ export default function Login() {
 };
 
   // ================= GOOGLE LOGIN =================
-  // const handleGoogleSuccess = async (
-  //   credentialResponse
-  // ) => {
-  //   try {
-  //     const res = await axios.post(
-  //       `${API_URL}/api/auth/google`,
-  //       {
-  //         token:
-  //           credentialResponse.credential,
-  //       }
-  //     );
-
-  //     localStorage.setItem(
-  //       "token",
-  //       res.data.token
-  //     );
-
-  //     localStorage.setItem(
-  //       "user",
-  //       JSON.stringify(res.data.user)
-  //     );
-
-  //     navigate("/dashboard");
-  //   } catch (err) {
-  //     alert(
-  //       err.response?.data?.message ||
-  //         "Google login failed."
-  //     );
-  //   }
-  // };
-
   const handleGoogleSuccess = async (credentialResponse) => {
   try {
     setError("");
