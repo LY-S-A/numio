e783ce0b68c003449b0e1368950518feb3252086
lysa.waitlist <Lysauniverse30@gmail.com>
2026-08-22 13:18:59 +0100

    Update FooterStats.jsx

--- src/components/FooterStats.jsx
+++ src/components/FooterStats.jsx
@@ -1,33 +1,3 @@
-// import "../styles/components.css";
-
-// const FooterStats = () => {
-//   return (
-//     <div className="footer-stats">
-//       <div>
-//         <span>System Status</span>
-//         <h4>All Systems Operational</h4>
-//       </div>
-
-//       <div>
-//         <span>SMS Delivery</span>
-//         <h4>98.7%</h4>
-//       </div>
-
-//       <div>
-//         <span>Numbers Available</span>
-//         <h4>125,000+</h4>
-//       </div>
-
-//       <div>
-//         <span>Average Delivery</span>
-//         <h4>3.2s</h4>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FooterStats;
-
 import "../styles/components.css";
 
 const FooterStats = () => {
